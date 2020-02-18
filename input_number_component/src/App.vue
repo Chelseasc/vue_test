2388e8a16bca186d92da1086f5b7db9c132e15c3
Chelseasc <sshencchen@sina.com>
2020-02-19 00:07:15 +0800

    数字输入框组件

---
+++ input_number_component/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <h1>数字输入框组件</h1>
+    <h4>用来快捷输入一个标准的数字</h4>
+    <p>子组件的数：{{total}}</p>
+    <InputNumbers 
+      @increase="handleTotal"
+      @reduce="handleTotal"
+      @input="handleTotal"
+    />
+  </div>
+</template>
+
+<script>
+import InputNumbers from './components/InputNumbers.vue'
+export default {
+  components: {
+    InputNumbers
+  },
+  data() {
+    return {
+      total: 0
+    }
+  },
+  methods: {
+    handleTotal(total) {
+      this.total = total;
+    }
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ input_number_component/src/components/InputNumbers.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="content">
+    <input type="text" :value="count" @change="handleInput">
+    <button @click="handleIncrease">+</button>
+    <button @click="handleReduce">-</button>
+    当前：{{count}}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    handleIncrease() {
+      this.count++;
+      this.$emit('increase', this.count);
+    },
+    handleReduce() {
+      this.count--;
+      this.$emit('reduce', this.count);
+    },
+    handleInput(event) {
+      const val = event.target.value;
+      this.$emit('input', val);
+      this.count = val;      
+    }
+
+  }
+}
+</script>
+
+<style scoped>
+.content {
+  width: 400px;
+  height: 50px;
+  border: 1px solid #000;
+  padding: 20px;
+  margin: 0 auto;
+}
+</style>
